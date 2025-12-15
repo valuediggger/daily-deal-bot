ad5eb0ca8794be780b45b34c1ccf8eb9b58ff058
Value Digger <76144760+valuediggger@users.noreply.github.com>
2025-12-15 19:54:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import os
 
-genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 print("Checking available models...")
 for m in genai.list_models():
